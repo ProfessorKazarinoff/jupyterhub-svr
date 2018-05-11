3cda4f55c31529007b58ec815634e71b61a52b80
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-05-11 15:07:04 -0700

    Create jupyterhub_config_OAuth.py

---
+++ jupyterhub_config_OAuth.py
@@ -0,0 +1,10 @@
+
+from oauthenticator.google import GoogleOAuthenticator
+c.JupyterHub.authenticator_class = GoogleOAuthenticator
+
+c.GoogleOAuthenticator.hosted_domain = 'pcc.edu'
+c.GoogleOAuthenticator.login_service = 'Portland Community College'
+
+# fill out client secret and client id in ./env
+#    $ source ./env to get github environment variables
+#    $ sudo full/path/to/jupyterhub
